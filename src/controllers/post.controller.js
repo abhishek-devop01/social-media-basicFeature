67af5321897b1448e90f24b2bc9c488a4c00cc1f
abhishek devop <standup787059@gmail.com>
2025-08-14 22:00:26 +0530

    AI integrated

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -3,4 +3,10 @@ const postModel = require('../models/post.model')
 
 async function createPostController(req, res){
      const file = req.file;
+     console.log('file recieved', file);
+     
+}
+
+module.exports = {
+     createPostController
 }
